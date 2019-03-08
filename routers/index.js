053d7642427c3fd5bd1323e02670be5ab8b9ea0b
zyb955140 <384815317@qq.com>
2019-03-08 17:12:40 +0800

    添加USDT代币转账

---
+++ routers/index.js
@@ -0,0 +1,25 @@
+const router = require('koa-router')()
+
+const accountController = require("../controllers/account")
+const transactionController = require("../controllers/transaction")
+const tokenController = require("../controllers/token")
+
+
+
+router.get('/', accountController.getAccountList);
+
+router.post('/', (ctx) => {
+
+    const body = ctx.request.body;
+  
+    ctx.body = 'Hello Router POST '+ JSON.stringify(body);
+    });
+router.post('/token/send', tokenController.sendTokenTransaction)
+
+router.post('/account/create',accountController.createAccount)
+router.post('/account/privatekey',accountController.getAccountByPrivatekey)
+router.post('/account/keystore',accountController.getAccountByKeystore)
+
+router.get('/transaction', transactionController.transaction)
+router.post('/transaction/send', transactionController.sendTransaction)
+module.exports = router
---
+++ utils/web3helper.js
@@ -0,0 +1,16 @@
+const Web3 = require('web3');
+
+module.exports = {
+    getWeb3(){
+        const web3 = new Web3(Web3.givenProvider || "https://mainnet.infura.io/v3/de08a5f2792f46b1a26d74b31ac9d0e3");
+        return web3
+    },
+    getContract(){
+        const web3 = module.exports.getWeb3()
+        //定义合约ABI
+        const contractAbi =[{"anonymous": false,"inputs": [{"indexed": true,"name": "_owner","type": "address"},{"indexed": true,"name": "_spender","type": "address"},{"indexed": false,"name": "_value","type": "uint256"}],"name": "Approval","type": "event"},{"anonymous": false,"inputs": [{"indexed": true,"name": "_from","type": "address"},{"indexed": true,"name": "_to","type": "address"},{"indexed": false,"name": "_value","type": "uint256"}],"name": "Transfer","type": "event"},{"constant": false,"inputs": [{"name": "_spender","type": "address"},{"name": "_value","type": "uint256"}],"name": "approve","outputs": [{"name": "success","type": "bool"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": false,"inputs": [{"name": "_to","type": "address"},{"name": "_value","type": "uint256"}],"name": "transfer","outputs": [{"name": "success","type": "bool"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": false,"inputs": [{"name": "_from","type": "address"},{"name": "_to","type": "address"},{"name": "_value","type": "uint256"}],"name": "transferFrom","outputs": [{"name": "success","type": "bool"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"inputs": [{"name": "_name","type": "string"},{"name": "_symbol","type": "string"},{"name": "_decimals","type": "uint8"},{"name": "_totalSupply","type": "uint256"}],"payable": false,"stateMutability": "nonpayable","type": "constructor"},{"constant": true,"inputs": [{"name": "_owner","type": "address"},{"name": "_spender","type": "address"}],"name": "allowance","outputs": [{"name": "remaining","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": true,"inputs": [{"name": "_owner","type": "address"}],"name": "balanceOf","outputs": [{"name": "balance","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": true,"inputs": [],"name": "decimals","outputs": [{"name": "","type": "uint8"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": true,"inputs": [],"name": "name","outputs": [{"name": "","type": "string"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": true,"inputs": [],"name": "symbol","outputs": [{"name": "","type": "string"}],"payable": false,"stateMutability": "view","type": "function"},{"constant": true,"inputs": [],"name": "totalSupply","outputs": [{"name": "","type": "uint256"}],"payable": false,"stateMutability": "view","type": "function"}]
+        let contractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+        let myContract = new web3.eth.Contract(contractAbi, contractAddress)
+        return myContract
+    }
+}
